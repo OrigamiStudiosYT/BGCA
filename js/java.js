0fd173935ba681c09c9fd9749062ae05b63564b6
Jeremy Patrick <JeremyPatrick@student.avrtech.com>
2019-05-31 12:37:53 -0400

    Commented JS

--- js/java.js
+++ js/java.js
@@ -1,4 +1,30 @@
-// When the user scrolls down 20px from the top of the document, show the button
+/* ******************************************************************************
+    *
+    *   Program: Java.js
+    *
+    *   Description: Handles the Javascript for the page
+    *
+    *   Author:  Jeremy Patrick, Andrew Lortie, Connor Hurley
+    *
+    *   Date: 5/31/2019
+    *
+    *   History: 5/29/2019 Creation
+    *
+    ****************************************************************************** */
+
+/* ******************************************************************************
+    *
+    *   Module: scrollFunction
+    *
+    *   Description: When the user scrolls down 20px from the top of the document, show the button
+    *
+    *   Author:  Andrew Lortie
+    *
+    *   Date: 5/31/2019
+    *
+    *   History: 5/29/2019 Creation
+    *
+    ****************************************************************************** */
 window.onscroll = function() {dotHandler()};
 
 function scrollFunction() 
@@ -13,14 +39,38 @@ function scrollFunction()
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+/* ******************************************************************************
+    *
+    *   Module: topFunction
+    *
+    *   Description: When the user clicks on the button, scroll to the top of the document
+    *
+    *   Author:  Andrew Lortie
+    *
+    *   Date: 5/31/2019
+    *
+    *   History: 5/29/2019 Creation
+    *
+    ****************************************************************************** */
 function topFunction() 
 {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }   
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* ******************************************************************************
+    *
+    *   Module: myFunction
+    *
+    *   Description: Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+    *
+    *   Author:  Andrew Lortie
+    *
+    *   Date: 5/31/2019
+    *
+    *   History: 5/29/2019 Creation
+    *
+    ****************************************************************************** */
 function myFunction() 
 {
     var x = document.getElementById("myTopnav");
@@ -34,7 +84,19 @@ function myFunction()
     }
 }
 
-// Following code obtained from Connor Hurley
+/* ******************************************************************************
+    *
+    *   Module: dotHandler
+    *
+    *   Description: Controls the dynamic color change of the dots in index.
+    *
+    *   Author:  Jeremy Patrick, Connor Hurley
+    *
+    *   Date: 5/31/2019
+    *
+    *   History: 5/31/2019 Creation
+    *
+    ****************************************************************************** */
 function dotHandler()
     {
         var top = document.getElementById("top");
